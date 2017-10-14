82e0bf4db52f96c05231747c2ff839ce3e81b5dd
arsaol <arieladsalazar@gmail.com>
2017-10-14 19:56:04 -0400

    ejemplo de conexion a php

---
+++ conexionx.php
@@ -0,0 +1,22 @@
+<?php
+ 
+$host='localhost';
+$db = 'postgres';
+$username = 'postgres';
+$password = 'postgres';
+$dsn = "pgsql:host=$host;port=5432;dbname=$db;user=$username;password=$password";
+ 
+ 
+try{
+    // create a PostgreSQL database connection
+    $conn = new PDO($dsn);
+ 
+    // display a message if connected to the PostgreSQL successfully
+    if($conn){
+        echo "Connected to the <strong>$db</strong> database successfully!";
+    }
+}catch (PDOException $e){
+    // report error message
+    echo $e->getMessage();
+}
+?>
